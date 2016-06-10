ef0317663533f797627d49d8f5a36f0889ab3e0c
Justin <justin_ellery@hotmail.ca>
2016-06-09 23:49:52 -0400

    Working on Project-01

--- assignments/project-01/index.php
+++ assignments/project-01/index.php
@@ -1,129 +1,90 @@
 <!DOCTYPE HTML>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.0.2/css/hover-min.css">
     <link rel="stylesheet" href="styles.css">
-    <title>Hey</title>
+    <title>Sports Database</title>
+
 </head>
+
 <body>
+
 
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Brand</a>
             </div>
             <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
+                <ul class="nav navbar-nav navbar-left">
+                    <!-- NHL -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#"><img src="images/nhl.png" style="height:45px;width:auto;" /></a>
+                    </li>
+                    <! -- NBA -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#">
+                            <img src="images/nba.png" style="height:45px;width:auto;" />
+                        </a>
+                    </li>
+                    <! -- NFL -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#">
+                            <img src="images/nfl.png" style="height:45px;width:auto;" />
+                        </a>
                     </li>
                 </ul>
-                <form class="navbar-form navbar-left" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
+                    <!-- CFL -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#">
+                            <img src="images/cfl.png" style="height:45px;width:auto;" />
+                        </a>
+                    </li>
+                    <!-- NLL -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#">
+                            <img src="images/nll.png" style="height:45px;width:auto;" />
+                        </a>
+                    </li>
+                    <!-- MLB -->
+                    <li>
+                        <a class="hvr-bounce-in" href="#">
+                            <img src="images/mlb.png" style="height:auto;width:70px;" />
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-
-
-    <section>
-        <div class="container">
-
-            <div class="row">        
-                <div class="col-md-4">
-                    <img src="images/nhl.png" class="center-block img-responsive logo" style="height:300px;width:auto;">
-                </div>
-                <div class="col-md-4">
-                        <img src="images/nba.png" class="center-block img-responsive logo" alt="Cinque Terre" style="height:300px;width:auto;">
-                    </div>
-                <div class="col-md-4 ">
-                    <img src="images/nfl.png" class="center-block img-responsive logo" style="height:300px;width:auto;">
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <h2>Welcome to the Professional Sports Database for Professionals.</h2>
-                    <h3>What you're about to see is confidential and you are to tell nobody.</h3>
-                    <h1>GOT IT Kid?</h1>
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="row">        
-                <div class="col-md-4">
-                    <img src="images/cfl.png" class="center-block img-responsive logo" style="height:300px;width:auto;">
-                </div>
-                <div class="col-md-4">
-                        <img src="images/nll.png" class="center-block img-responsive logo" alt="Cinque Terre" style="height:300px;width:auto;">
-                    </div>
-                <div class="col-md-4 ">
-                    <img src="images/mlb.png" class="center-block img-responsive logo" style="height:250px;width:auto;">
-                </div>
-            </div>
-
-        </div>
+    <section id="splash">
     </section>
 
+    <div id="footer">
+        <p style="text-align:right; margin-right:20px;">Copyright Justin Ellery 2016©</p>
+    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+    <script>
+        $.backstretch(["images/bg5.jpg",
+                       "images/bg2.jpg",
+                        "images/bg3.jpg",
+                        "images/bg4.jpg"], {
+            duration: 3000,
+            fade: 750
+        });
+    </script>
 </body>
+
 </html>
